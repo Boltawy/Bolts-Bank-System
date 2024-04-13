174273a784bef1f114eeafe9341d8d6431c8b58f
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2024-04-13 10:25:25 +0200

    Revert "Update FileManager.h"
    This reverts commit 8ae67fd8fe2c6a1ed4dd0726b59244b39feec58b.

--- FileManager.h
+++ FileManager.h
@@ -1,5 +1,5 @@
 #pragma once
-//#include "DataSourceInterface.h"
+#include "DataSourceInterface.h"
 #include "Client.h"
 #include "Employee.h"
 #include "Admin.h"
@@ -14,78 +14,74 @@ public:
 	static fstream employeeFile;
 	static fstream adminFile;
 
-
-
-	//Probably junk code below this line======================================\
-	// 
 	//Validation for objects before adding them to database.
 
-	//static bool isValidClient(Client& obj)
-	//{
-	//	if (
-	//		Validation::isValidName(obj.getName()) &&
-	//		Validation::isValidPassword(obj.getPassword()) &&
-	//		Validation::isValidBalance(obj.getBalance())
-	//		)
-	//	{
-	//		return true;
-	//	}
-	//	return false;
-	//}
-	//static bool isValidEmployee(Employee& obj)
-	//{
-	//	if (
-	//		Validation::isValidName(obj.getName()) &&
-	//		Validation::isValidPassword(obj.getPassword()) &&
-	//		Validation::isValidSalary(obj.getSalary())
-	//		)
-	//	{
-	//		return true;
-	//	}
-	//	return false;
-	//}
-	//static bool isValidAdmin(Admin& obj)
-	//{
-	//	if (
-	//		isValidEmployee(obj)
-	//		)
-	//	{
-	//		return true;
-	//	}
-	//	return false;
-	//}
+	static bool isValidClient(Client& obj)
+	{
+		if (
+			Validation::isValidName(obj.getName()) &&
+			Validation::isValidPassword(obj.getPassword()) &&
+			Validation::isValidBalance(obj.getBalance())
+			)
+		{
+			return true;
+		}
+		return false;
+	}
+	static bool isValidEmployee(Employee& obj)
+	{
+		if (
+			Validation::isValidName(obj.getName()) &&
+			Validation::isValidPassword(obj.getPassword()) &&
+			Validation::isValidSalary(obj.getSalary())
+			)
+		{
+			return true;
+		}
+		return false;
+	}
+	static bool isValidAdmin(Admin& obj)
+	{
+		if (
+			isValidEmployee(obj)
+			)
+		{
+			return true;
+		}
+		return false;
+	}
 
-	//void addClient(Client obj)
-	//{
-	//	if (isValidClient(obj))
-	//	{
-	//		clientFile.open("Client_Database.txt", ios::app);
-	//		if (clientFile.is_open())
-	//		{
-	//			clientFile << to_string(obj.getID()) + '#' + obj.getName() + '#' + obj.getPassword() + '#' + to_string(obj.getBalance()) << endl;
-	//		}
-	//		clientFile.close();
-	//	}
-	//	else
-	//	{
-	//		cout << "Client has invalid Properties.";
-	//	}
-	//}
-	// void addEmployee(Employee& obj)
-	// {
-	//	 if (isValidEmployee(obj))
-	//	 {
-	//		 employeeFile.open("Employee_Database.txt", ios::app);
-	//		 if (employeeFile.is_open())
-	//		 {
-	//			 employeeFile << to_string(obj.getID()) + '#' + obj.getName() + '#' + obj.getPassword() + '#' + to_string(obj.getSalary()) << endl;
-	//		 }
-	//		 employeeFile.close();
-	//	 }
-	//	 else
-	//	 {
-	//		 cout << "Employee has invalid Properties.";
-	//	 }
+	void addClient(Client obj)
+	{
+		if (isValidClient(obj))
+		{
+			clientFile.open("Client_Database.txt", ios::app);
+			if (clientFile.is_open())
+			{
+				clientFile << to_string(obj.getID()) + '#' + obj.getName() + '#' + obj.getPassword() + '#' + to_string(obj.getBalance()) << endl;
+			}
+			clientFile.close();
+		}
+		else
+		{
+			cout << "Client has invalid Properties.";
+		}
+	}
+	 void addEmployee(Employee& obj)
+	 {
+		 if (isValidEmployee(obj))
+		 {
+			 employeeFile.open("Employee_Database.txt", ios::app);
+			 if (employeeFile.is_open())
+			 {
+				 employeeFile << to_string(obj.getID()) + '#' + obj.getName() + '#' + obj.getPassword() + '#' + to_string(obj.getSalary()) << endl;
+			 }
+			 employeeFile.close();
+		 }
+		 else
+		 {
+			 cout << "Employee has invalid Properties.";
+		 }
 		 
 	 //}
 	 //void addAdmin(Admin& obj) 
