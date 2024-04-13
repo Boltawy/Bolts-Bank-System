cdf3049be08137dd1819bc03b0c888621dd2277c
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2024-04-13 10:25:38 +0200

    Reapply "Update FileManager.h"
    This reverts commit 174273a784bef1f114eeafe9341d8d6431c8b58f.

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	//testing readFromDatabase
-	//Client x("Micheal", "Ab12345678", 1, 5000);
+	Client x("Micheal", "Ab12345678", 1, 5000);
 	//Client x2("Micheal Schwartz", "Ab12345678", 2, 5000);
 	//Client x3("Micheal Schwartz", "Ab12345678", 3, 5000);
 	//Client x4("Micheal Schwartz", "Ab12345678", 4, 5000);
@@ -23,7 +23,7 @@ int main()
 	//z.addClient(x5);
 	// 
 	//testing split and reading from client database.
-	cout << Parser::split(Parser::getClientByID(1))[3];
+	//cout << Parser::split(Parser::getClientByID(1))[3];
 
 	//testing reading from a file
 	//string line = "1#Micheal#Ab12345678#5000.000000";
