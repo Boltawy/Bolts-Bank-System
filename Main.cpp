fb1fb469efcbf80134f7e305bd240b90fbe92855
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2024-04-18 10:23:50 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,20 +7,20 @@ using namespace std;
 
 int main()
 {
-	//testing readFromDatabase
+	//testing client ID generation
+	Employee z("Franklin", "87654321", 100000);
 	Client x("Micheal", "Ab12345678", 5000);
-	//Client x2("Micheal Schwartz", "Ab12345678", 2, 5000);
-	//Client x3("Micheal Schwartz", "Ab12345678", 3, 5000);
-	//Client x4("Micheal Schwartz", "Ab12345678", 4, 5000);
-	//Client x5("Micheal Schwartz", "Ab12345678", 5, 5000);
-	//Employee z("Franklin", "87654321", 3, 100000);
+	z.addClient(x);
+	Client x2("Micheal Schwartz", "Ab12345678", 5000);
+	z.addClient(x2);
+	Client x3("Micheal Schwartz", "Ab12345678", 5000);
+	z.addClient(x3);
+	Client x4("Micheal Schwartz", "Ab12345678", 5000);
+	z.addClient(x4);
+	Client x5("Micheal Schwartz", "Ab12345678", 5000);
+	z.addClient(x5);
 
 
-	//z.addClient(x);
-	//z.addClient(x2);
-	//z.addClient(x3);
-	//z.addClient(x4);
-	//z.addClient(x5);
 	// 
 	//testing split and reading from client database.
 	//cout << Parser::split(Parser::getClientByID(1))[3];
@@ -28,4 +28,8 @@ int main()
 	//testing reading from a file
 	//string line = "1#Micheal#Ab12345678#5000.000000";
 	//cout << stoi(line.substr(0, line.find('#')));
+
+
+	//testing getLastID from Client
+	//cout << Client::getLastID();
 }
