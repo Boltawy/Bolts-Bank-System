dcde81aaed4d9e643ef0c3e4bfcaad52ca610afe
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2024-04-26 12:19:18 +0300

    Added a PERFECT Welcome Screen

--- Screens.h
+++ Screens.h
@@ -5,6 +5,37 @@ using namespace std;
 class Screens
 {
 public:
+
+
+	static void welcomeScreen()
+	{
+		cout << "\n    #=========================================================================================================#\n"
+			<< "    |  Bolt Proudly Presents........                                                                          |\n"
+			<< "    |         ________                                                                           ________     |\n"
+			<< "    |        /       /       BBBBBBBBBBBBBBBBB   BBBBBBBBBBBBBBBBB      SSSSSSSSSSSSSSS         /       /     |\n"
+			<< "    |       /       /        B::::::::::::::::B  B::::::::::::::::B   SS:::::::::::::::S       /       /      |\n"
+			<< "    |      /__     /__       B::::::BBBBBB:::::B B::::::BBBBBB:::::B S:::::SSSSSS::::::S      /__     /__     |\n"
+			<< "    |        /       /       BB:::::B     B:::::B B:::::B     B:::::BS:::::S     SSSSSSS        /       /     |\n"
+			<< "    |        /      /          B::::B     B:::::B  B::::B     B:::::BS:::::S                    /      /      |\n"
+			<< "    |        /     /           B::::B     B:::::B  B::::B     B:::::BS:::::S                    /     /       |\n"
+			<< "    |        /    /            B::::BBBBBB:::::B   B::::BBBBBB:::::B  S::::SSSS                 /    /        |\n"
+			<< "    |        /   /             B:::::::::::::BB    B:::::::::::::BB    SS::::::SSSSS            /   /         |\n"
+			<< "    |        /  /              B::::BBBBBB:::::B   B::::BBBBBB:::::B     SSS::::::::SS          /  /          |\n"
+			<< "    |        / /               B::::B     B:::::B  B::::B     B:::::B       SSSSSS::::S         / /           |\n"
+			<< "    |        //                B::::B     B:::::B  B::::B     B:::::B       SSSSSS::::S         //            |\n"
+			<< "    |        /                 B::::B     B:::::B  B::::B     B:::::B       SSSSSS::::S         /             |\n"
+			<< "    |                       BB:::::BBBBBB::::::B B:::::BBBBBB::::::BSSSSSSS     S:::::S                       |\n"
+			<< "    |                       B:::::::::::::::::B B:::::::::::::::::B S::::::SSSSSS:::::S                       |\n"
+			<< "    |                       B::::::::::::::::B  B::::::::::::::::B  S:::::::::::::::SS                        |\n"
+			<< "    |                       BBBBBBBBBBBBBBBBB   BBBBBBBBBBBBBBBBB    SSSSSSSSSSSSSSS                          |\n"
+			<< "    |                                                                                                         |\n"
+			<< "    |                                                                             ........Bolt's Bank System  |\n"
+			<< "    *=========================================================================================================*\n"
+			<< "                             || Our MOTTO: Give us your money, And Never see it again. ||                      \n"
+			<< "                               ========================================================                        \n";
+	}
+
+
 	static void runApp()
 	{
 		//Load Databases on open.
@@ -12,6 +43,7 @@ public:
 		FilesHelper::getAllEmployees();
 		FilesHelper::getAllAdmins();
 
+		welcomeScreen();
 
 		//Update Databases on close.
 		FilesHelper::updateClientFile();
